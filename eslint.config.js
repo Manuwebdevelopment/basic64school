d1186eefb995277609faea6b1b603f0296a3f8a4
Manuwebdevelopment <manu.webdevelopment@gmail.com>
2026-05-20 20:50:10 +1000

    fix: add proper ESLint v9 flat config for Next.js

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const nextPlugin = require('eslint-config-next/turbo');
+
+module.exports = [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...nextPlugin,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+];
